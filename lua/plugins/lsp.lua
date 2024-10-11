b2c9ad75a5b43207f9ae1c423e686f6babc1700a
r <reginaldbeakes@gmail.com>
2024-10-10 20:30:44 -0400

    init

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,201 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		{
+			"hrsh7th/nvim-cmp",
+			dependencies = {
+				{ "hrsh7th/cmp-buffer" },
+				{ "hrsh7th/cmp-path" },
+				{ "hrsh7th/cmp-nvim-lua" },
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "onsails/lspkind-nvim" },
+				{ "saadparwaiz1/cmp_luasnip", dependencies = { "L3MON4D3/LuaSnip" } },
+				{ "tamago324/cmp-zsh" },
+			},
+		},
+
+		"jose-elias-alvarez/nvim-lsp-ts-utils",
+		"b0o/schemastore.nvim",
+		"j-hui/fidget.nvim",
+	},
+	config = function()
+		-- LSP Keymap
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc)
+					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+
+				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+				map("K", vim.lsp.buf.hover, "Hover Documentation")
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+			end,
+		})
+
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
+
+		local servers = {
+			bashls = true,
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							checkThirdParty = false,
+						},
+					},
+				},
+			},
+			pyright = true,
+			ruff_lsp = true,
+			gopls = {
+				settings = {
+					gopls = {
+						codelenses = { test = true },
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						} or nil,
+					},
+				},
+
+				flags = {
+					debounce_text_changes = 200,
+				},
+			},
+			rust_analyzer = {
+				cmd = {
+					"rustup",
+					"run",
+					"nightly",
+					"rust-analyzer",
+				},
+			},
+			tsserver = {
+				init_options = require("nvim-lsp-ts-utils"),
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
+
+				on_attach = function(client)
+					ts_util.setup({ auto_inlay_hints = false })
+					ts_util.setup_client(client)
+				end,
+			},
+			-- C#
+			omnisharp = {
+				cmd = {
+					vim.fn.expand("~/build/omnisharp/run"),
+					"--languageserver",
+					"--hostPID",
+					tostring(vim.fn.getpid()),
+				},
+			},
+			html = true,
+			cssls = true,
+			tailwindcss = true,
+			vimls = true,
+			yamlls = true,
+			jsonls = {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			},
+		}
+
+		require("fidget").setup({})
+		require("mason").setup()
+
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, { "lua_ls", "jsonls" })
+
+		require("mason-lspconfig").setup({
+			ensure_installed = ensure_installed,
+			handlers = {
+				function(server_name) -- handle everything here
+					local server = servers[server_name] or {} -- grab the server or empty table
+					if type(server) ~= "table" then
+						server = {}
+					end -- if the server is not a table then make it an empty table
+					require("lspconfig")[server_name].setup(server) -- setup the server
+				end,
+			},
+		})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			-- completion = { completeopt = "menu,menuone,noinsert" },
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				-- ["<C-l>"] = cmp.mapping(function()
+				--   if luasnip.expand_or_locally_jumpable() then
+				--     luasnip.expand_or_jump()
+				--   end
+				-- end, { "i", "s" }),
+				-- ["<C-h>"] = cmp.mapping(function()
+				--   if luasnip.locally_jumpable(-1) then
+				--     luasnip.jump(-1)
+				--   end
+				-- end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+
+		vim.diagnostic.config({
+			update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
+}
